1dd2121862a2d606458e5ed64097e05817b6a9bc
KseniiaTr <ks31993@mail.ru>
2023-01-03 18:02:56 -0800

    Correction

--- tests/test_demoqa_hw.py
+++ tests/test_demoqa_hw.py
@@ -10,18 +10,26 @@ def test_demoqa_hw(size_browser):
         '[id^=google_ad][id$=__container__').should(have.size_greater_than_or_equal(3)).perform(command.js.remove)
 
 
-    browser.element('[id="firstName"]').type('Alex').press_enter()
-    browser.element('[id="lastName"]').type('Alekseev').press_enter()
-    browser.element('[id="userEmail"]').type('alexalekseev@gmail.com').press_enter()
+    browser.element('[id="firstName"]').type('Alex')
+    browser.element('[id="lastName"]').type('Alekseev')
+    browser.element('[id="userEmail"]').type('alexalekseev@gmail.com')
 
     browser.element('.custom-control-label').should(have.exact_text('Male')).click()
-    browser.element('[id="userNumber"]').type('89101231212')
+    browser.element('[id="userNumber"]').type('999101231212')
 
-    browser.element('[id="dateOfBirthInput"]').perform(command.js.set_value('12/02/1990'))
+    browser.element('[id="dateOfBirthInput"]').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('[value="4"]').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('[value="1991"]').click()
+    browser.element('.react-datepicker__day--007').click()
+
+
     browser.element('[id="subjectsInput"]').type('History').press_enter()
     browser.element('[for=hobbies-checkbox-3]').click()
 
-    browser.element('[id="uploadPicture"]').send_keys('/Users/caseymac/Desktop/Photo_test_yellow.png')
+    browser.element('[id="uploadPicture"]').send_keys(
+        '/Users/caseymac/PycharmProjects/KseniiaTr/qa_gury_python_3_5/Photo_test_yellow.png')
 
 
     browser.element('[id="currentAddress"]').type('Moscow, Pyatnitskaya st 12/12').press_enter()
@@ -31,6 +39,26 @@ def test_demoqa_hw(size_browser):
 
 
     browser.element('[id="submit"]').press_enter()
+
+
+def test_correct_info():
+    browser.element('.table-responsive').should(have.text(
+                                               'Alex',
+                                               'Alekseev',
+                                               'alexalekseev@gmail.com',
+                                               'Male',
+                                               '89101231212',
+                                               '07 May, 1991',
+                                               'History'
+                                               'Music',
+                                               'Photo_test_yellow.png',
+                                               'Moscow, Pyatnitskaya st 12/12',
+                                               'NCR Delhi'))
+
+
+
+
+
 
 
 
