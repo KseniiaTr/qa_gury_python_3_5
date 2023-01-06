0dd889278b240c960eb38ec8651e184f3e684413
KseniiaTr <ks31993@mail.ru>
2023-01-05 18:34:12 -0800

    Добавлены исправления

--- tests/test_demoqa_hw.py
+++ tests/test_demoqa_hw.py
@@ -1,6 +1,7 @@
 from selene.support.shared import browser
-from selene import be, have, command
+from selene import have
 from selene import command
+import os
 
 
 def test_demoqa_hw(size_browser):
@@ -10,39 +11,41 @@ def test_demoqa_hw(size_browser):
         '[id^=google_ad][id$=__container__').should(have.size_greater_than_or_equal(3)).perform(command.js.remove)
 
 
-    browser.element('[id="firstName"]').type('Alex')
-    browser.element('[id="lastName"]').type('Alekseev')
-    browser.element('[id="userEmail"]').type('alexalekseev@gmail.com')
+    browser.element('#firstName').type('Alex')
+    browser.element('#lastName').type('Alekseev')
+    browser.element('#userEmail').type('alexalekseev@gmail.com')
 
-    browser.element('.custom-control-label').should(have.exact_text('Male')).click()
-    browser.element('[id="userNumber"]').type('999101231212')
+    browser.all('[name=gender]').element_by(have.value('Male')).element('..').click()
+    browser.element('#userNumber').type('89101231212')
 
-    browser.element('[id="dateOfBirthInput"]').click()
-    browser.element('.react-datepicker__month-select').click()
-    browser.element('[value="4"]').click()
-    browser.element('.react-datepicker__year-select').click()
-    browser.element('[value="1991"]').click()
-    browser.element('.react-datepicker__day--007').click()
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select').send_keys('May')
+    browser.element('.react-datepicker__year-select').send_keys('1991')
+    browser.element(f'.react-datepicker__day--00{7}').click()
 
 
-    browser.element('[id="subjectsInput"]').type('History').press_enter()
-    browser.element('[for=hobbies-checkbox-3]').click()
-
-    browser.element('[id="uploadPicture"]').send_keys(
-        '/Users/caseymac/PycharmProjects/KseniiaTr/qa_gury_python_3_5/Photo_test_yellow.png')
+    browser.element('#subjectsInput').type('History').press_enter()
+    browser.element('[type=checkbox][id=hobbies-checkbox-3]+label').should(
+        have.exact_text('Music')).click()
 
 
-    browser.element('[id="currentAddress"]').type('Moscow, Pyatnitskaya st 12/12').press_enter()
+    browser.element('#uploadPicture').set_value(
+        os.path.abspath(
+            os.path.join(os.path.dirname(__file__), 'resources/Photo_test_yellow.png')
+        )
+    )
 
-    browser.element('[id="react-select-3-input"]').type('NCR').press_enter()
-    browser.element('[id="react-select-4-input"]').type('Delhi').press_enter()
+    browser.element('#currentAddress').perform(command.js.set_value(
+        'Delhi, Pyatnitskaya st 12/12'))
+    browser.element('#react-select-3-input').send_keys('NCR').press_enter()
+    browser.element('#react-select-4-input').send_keys('Delhi').press_enter()
 
 
-    browser.element('[id="submit"]').press_enter()
+    browser.element('#submit').perform(command.js.click)
 
 
-def test_correct_info():
-    browser.element('.table-responsive').should(have.text(
+def test_check_info_in_table():
+    browser.element('.table-responsive').all('td').even.should(have.exact_text(
                                                'Alex',
                                                'Alekseev',
                                                'alexalekseev@gmail.com',
